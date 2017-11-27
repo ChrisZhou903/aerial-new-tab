99f1a693dbf51d6245d7a9de7433d4ff72798fa8
chriszhou903 <jasonzhou903@qq.com>
2017-11-28 01:14:21 +0800

    add videos list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,45 @@ import React, { PureComponent } from 'react'
 import entries from './constants/entries.json'
 import locationSvg from './assets/location.svg'
 
-const randomIndex = Math.floor(Math.random() * entries.length)
+const sortedEntries = entries.sort(
+  (a, b) => {
+    const nameA = a.accessibilityLabel.toLowerCase()
+    const nameB = b.accessibilityLabel.toLowerCase()
+
+    if (nameA < nameB) {
+      return -1
+    }
+
+    if (nameA > nameB) {
+      return 1
+    }
+
+    return 0
+  },
+)
+const randomIndex = Math.floor(Math.random() * sortedEntries.length)
 
 class App extends PureComponent {
   state = {
-    videos: entries || [],
+    listVisibility: false,
+    videos: sortedEntries || [],
     current: randomIndex || 0,
   }
 
+  handleLocationFocus = () => {
+    this.setState({
+      listVisibility: true,
+    })
+  }
+
+  handleLocationBlur = () => {
+    this.setState({
+      listVisibility: false,
+    })
+  }
+
   render() {
-    const { current, videos } = this.state
+    const { current, videos, listVisibility } = this.state
     const video = videos[current]
 
     return (
@@ -24,8 +53,20 @@ class App extends PureComponent {
           </video>
           <div className="video-info">
             <div className="video-location">
-              <img className="icon" src={locationSvg} alt="" />
-              {video.accessibilityLabel} - <span className="video-time">{video.timeOfDay}</span>
+              {listVisibility && <ul className="video-list">
+                {videos.map(v => (
+                  <li key={v.id}>{v.accessibilityLabel} - <span className="video-time">{v.timeOfDay}</span></li>
+                ))}
+              </ul>}
+              <div
+                tabindex="0"
+                className="video-location__cur"
+                onFocus={this.handleLocationFocus}
+                onBlur={this.handleLocationBlur}
+              >
+                <img className="icon" src={locationSvg} alt="" />
+                {video.accessibilityLabel} - <span className="video-time">{video.timeOfDay}</span>
+              </div>
             </div>
           </div>
         </div>
